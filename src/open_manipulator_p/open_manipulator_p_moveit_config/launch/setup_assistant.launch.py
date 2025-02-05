6d158205ed0e3b62adb544129c9526c506774ae2
legatalee <legatalee@gmail.com>
2025-02-05 14:51:46 +0900

    first commit

---
+++ src/open_manipulator_p/open_manipulator_p_moveit_config/launch/setup_assistant.launch.py
@@ -0,0 +1,7 @@
+from moveit_configs_utils import MoveItConfigsBuilder
+from moveit_configs_utils.launches import generate_setup_assistant_launch
+
+
+def generate_launch_description():
+    moveit_config = MoveItConfigsBuilder("open_manipulator_p", package_name="open_manipulator_p_moveit_config").to_moveit_configs()
+    return generate_setup_assistant_launch(moveit_config)
